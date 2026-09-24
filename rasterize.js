d3614a2260b2c25de33407d32d4551a47e179521
preetijoshi12 <joshipreetim@gmail.com>
2026-09-24 18:48:07 -0400

    Update rasterize.js

--- rasterize.js
+++ rasterize.js
@@ -156,7 +156,7 @@ function setupShaders() {
     // define fragment shader in essl using es6 template strings
     var fShaderCode = `
         void main(void) {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); // all fragments are white
+            gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0); // all fragments are blue
         }
     `;
     
